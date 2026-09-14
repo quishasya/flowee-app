7f56aa2051d3555ae2ba9d5122628b84b053048c
quish <quish123@gmail.com>
2026-09-14 08:40:23 +0700

    finishing home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flowee_app/data/dummy_data.dart';
 import 'package:flowee_app/models/flower.dart';
 import 'package:flowee_app/screens/detail_screen.dart';
+import 'package:flowee_app/widgets/flower_card.dart';
+import 'package:flowee_app/widgets/home_content_header.dart';
 import 'package:flowee_app/widgets/home_header.dart';
 import 'package:flutter/material.dart';
 
@@ -44,12 +46,45 @@ class _HomeScreenState extends State<HomeScreen> {
     
     return SafeArea(
       child: CustomScrollView(
+        // perantara/adapter antara widget ke customScrollView, ibarat ratu dan rakyat (harus ada perantara)
         slivers: [
           SliverToBoxAdapter(
-            child: HomeHeader(
-              onProfileTap: 
-            ),
-          )
+            child: HomeContentHeader(
+              selectedCategory: _selectedCategory, 
+              categories: _categories, 
+              // membaca apakah dia true/false
+              onQueryChanged: (value) => setState(() => _query = value), 
+              // _selectedCategory mengikuti value
+              onCategorySelected: (value) => setState(() => _selectedCategory = value),
+            )
+          ),
+          // jika bunga gaada di data maka screen gabisa di scroll, hanya menampilkan text
+          if (flowers.isEmpty)
+            SliverFillRemaining(
+              hasScrollBody: false,
+              child: Center(
+                child: Text('Bunga Tidak Ditemukan'),
+              ),
+            )
+          else 
+            SliverPadding(
+              padding: EdgeInsets.fromLTRB(20, 0, 20, 100),
+              sliver: SliverGrid(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
+                  childAspectRatio: 0.68
+                ),
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) => FlowerCard(
+                    flower: flowers[index], 
+                    onTap: () => _openDetail(flowers[index]),
+                  ),
+                  childCount: flowers.length
+                ), 
+              ),
+            )
         ],
       ),
     );
