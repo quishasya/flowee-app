dfc40bc4f62a42b7e36b1a8463bcb3f7446ff5e7
quish <quish123@gmail.com>
2026-09-23 10:48:18 +0700

    adding decorative glow widget

---
+++ lib/state/decorative_glow.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class DecorativeGlow extends StatelessWidget {
+  const DecorativeGlow({super.key, required this.size});
+
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return IgnorePointer(
+      child: Container(
+        width: size,
+        height: size,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          gradient: RadialGradient(
+            colors: [
+              Colors.white.withValues(alpha: 0.14),
+              Colors.white.withValues(alpha: 0.0)
+            ]
+          )
+        ),
+      ),
+    );
+  }
+}
